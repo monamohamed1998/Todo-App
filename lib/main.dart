46e04f0c11a695482b1aab8b2f3bca55decc04c6
mona <moona944198@gmai.com>
2024-08-05 00:35:47 +0300

    start todo app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todo/home_page.dart';
+
+void main() {
+  runApp(const Todo());
+}
+
+class Todo extends StatelessWidget {
+  const Todo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
