af531c04991d8c3deb5f1fdede3ae12bb1b654ef
mona <moona944198@gmai.com>
2024-08-05 16:16:47 +0300

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Todo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
